56c9a7fb091e2be35dd1176574a3fe14ebe690db
mohamad <mohamad.fathi13811@gmail.com>
2024-08-02 17:22:49 +0330

    coding edit and delete items + creating dashboard app

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from .forms import SignUpForm, LoginFrom, NewItemForm
+from .forms import SignUpForm, LoginFrom, NewItemForm, EditItemForm
 from core.models import Item, Category
 
 
@@ -66,8 +66,34 @@ def new(request):
 
     return render(request, 'core/new_item.html', {
         'form': form,
+        'title': 'New Item'
     })
 
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index_page')
+
+
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('detail_page', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'core/new_item.html', {
+        'form': form,
+        'title': 'Edit Item'
+    })
+
+
 # todo learn how to logout via django internal function
-
-
--- django_marketplace/urls.py
+++ django_marketplace/urls.py
@@ -4,17 +4,20 @@
 from django.conf import settings
 
 from django.contrib import admin
-from django.urls import path
-from core.views import index, detail, signup, new
+from django.urls import path, include
+from core.views import index, detail, signup, new, delete, edit
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from core.forms import LoginFrom
 
 urlpatterns = [
     path('', index, name='index_page'),
+    path('dashboard', include('dashboard.urls'), name='dashboard'),
     path('contact/', index, name='contact_page'),
     path('signup/', signup, name='signup_page'),
     path('new-item/', new, name='new_item_page'),
+    path('edit-item/<int:pk>', edit, name='edit_item_page'),
+    path('delete-item/<int:pk>', delete, name='delete_item'),
     path('login/', auth_views.LoginView.as_view(authentication_form=LoginFrom, template_name='core/login.html'), name='login_page'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('detail/<int:pk>/', detail, name='detail_page'),
